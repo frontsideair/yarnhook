900e15908b41c37974e2254b0ec8ac2e341a01c1
Fatih Altinok <photonia@gmail.com>
2017-12-17 18:00:50 +0300

    Add flags YARNHOOK_BYPASS and YARNHOOK_DEBUG

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ const findParentDir = require("find-parent-dir");
 const { execSync } = require("child_process");
 const { join } = require("path");
 
-// switch to gitdir
-const currentDir = process.cwd();
-const gitDir = findParentDir.sync(currentDir, ".git");
-console.log(currentDir, gitDir);
+const { YARNHOOK_BYPASS = false, YARNHOOK_DEBUG = false } = process.env;
 
-// run git diff HEAD@{1}..HEAD@{0} -- $CWD/yarn.lock
-const yarnLockPath = join(currentDir, yarn.lock);
-const output = execSync(`git diff HEAD@{1}..HEAD@{0} -- ${yarnLockPath}`, {
-  cwd: gitDir,
-  encoding: "utf-8"
-});
-console.log(output);
+if (!YARNHOOK_BYPASS) {
+  // switch to gitdir
+  const currentDir = process.cwd();
+  const gitDir = findParentDir.sync(currentDir, ".git");
 
-// if diff exists, run yarn install
-if (output.length > 0) {
-  execSync("yarn install");
+  if (YARNHOOK_DEBUG) {
+    console.log(currentDir, gitDir);
+  }
+
+  // run git diff HEAD@{1}..HEAD@{0} -- $CWD/yarn.lock
+  const yarnLockPath = join(currentDir, "yarn.lock");
+  const output = execSync(`git diff HEAD@{1}..HEAD@{0} -- ${yarnLockPath}`, {
+    cwd: gitDir,
+    encoding: "utf-8"
+  });
+
+  if (YARNHOOK_DEBUG) {
+    console.log(output);
+  }
+
+  // if diff exists, run yarn install
+  if (output.length > 0) {
+    const output = execSync("yarn install", { encoding: "utf-8" });
+    console.log(output);
+  }
 }
