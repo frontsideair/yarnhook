3667d698b147fd504aae73d1506e75246e70a68f
Allyson Beckers <abeckers@brighthealthplan.com>
2017-12-19 14:36:10 -0600

    add support for package-lock.json

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# yarnhook [![npm version](https://badge.fury.io/js/yarnhook.svg)](https://badge.fury.io/js/yarnhook)
+# lockfilehook [![npm version](https://badge.fury.io/js/lockfilehook.svg)](https://badge.fury.io/js/lockfilehook)
 
-`yarnhook` keeps your `node_modules` up-to-date when your `yarn.lock` changes
+`lockfilehook` keeps your `node_modules` up-to-date when your `yarn.lock` or `package-lock.json` changes
 due to git operations like `checkout`, `merge`, `rebase`, `pull` etc.
 
 # Installation
@@ -8,31 +8,33 @@ due to git operations like `checkout`, `merge`, `rebase`, `pull` etc.
 This package should be used with [husky](https://www.npmjs.com/package/husky).
 
 ```sh
-yarn add --dev yarnhook husky
+yarn add --dev lockfilehook husky
+# or 
+npm install --save-dev lockfilehook husky
 ```
 
 # Usage
 
-You should let `yarnhook` handle git hooks that change the dependencies. Example
+You should let `lockfilehook` handle git hooks that change the dependencies. Example
 `package.json` is as follows:
 
 ```json
 {
   "scripts": {
-    "postmerge": "yarnhook",
-    "postcheckout": "yarnhook",
-    "postrewrite": "yarnhook"
+    "postmerge": "lockfilehook",
+    "postcheckout": "lockfilehook",
+    "postrewrite": "lockfilehook"
   }
 }
 ```
 
 # Flags
 
-Prepend `YARNHOOK_BYPASS=true` to your git command if you don't want to run
-`yarn install` as a result, `YARNHOOK_DEBUG=true` to print debug information.
+Prepend `LOCKFILEHOOK_BYPASS=true` to your git command if you don't want to run
+`[yarn|npm] install` as a result, `LOCKFILEHOOK_DEBUG=true` to print debug information.
 
 An example:
 
 ```sh
-YARNHOOK_BYPASS=true git checkout feature-branch
+LOCKFILEHOOK_BYPASS=true git checkout feature-branch
 ```
--- index.js
+++ index.js
@@ -5,32 +5,58 @@
 const findParentDir = require("find-parent-dir");
 const { execSync } = require("child_process");
 const { join } = require("path");
+const fs = require('fs');
 
-const { YARNHOOK_BYPASS = false, YARNHOOK_DEBUG = false } = process.env;
+const { LOCKFILEHOOK_BYPASS = false, LOCKFILEHOOK_DEBUG = false } = process.env;
 
-if (!YARNHOOK_BYPASS) {
+if (!LOCKFILEHOOK_BYPASS) {
   // switch to gitdir
   const currentDir = process.cwd();
   const gitDir = findParentDir.sync(currentDir, ".git");
+  const yarnLockPath = join(currentDir, "yarn.lock");
+  const npmLockPath = join(currentDir, "package-lock.json");
 
-  if (YARNHOOK_DEBUG) {
-    console.log(currentDir, gitDir);
+  // check for yarn's and npm's lockfiles
+  let CMD = "";
+  const getLockfile = function() {
+    // get yarn's lockfile
+    if (fs.existsSync(yarnLockPath)) {
+      CMD = "yarn";
+      return yarnLockPath;
+    }
+
+    // get npm's lockfile
+    if (fs.existsSync(npmLockPath)) {
+      CMD = "npm";
+      return npmLockPath;
+    }
+
+    return null;
   }
 
-  // run git diff HEAD@{1}..HEAD@{0} -- $CWD/yarn.lock
-  const yarnLockPath = join(currentDir, "yarn.lock");
-  const output = execSync(`git diff HEAD@{1}..HEAD@{0} -- ${yarnLockPath}`, {
+  const lockfile = getLockfile();
+
+  if (LOCKFILEHOOK_DEBUG) {
+    console.log('currentDir:', currentDir);
+    console.log('gitDir:', gitDir);
+    console.log('lockfile:', lockfile);
+    console.log('CMD:', CMD);
+  }
+
+  // run a git diff on the lockfile
+  const output = execSync(`git diff HEAD@{1}..HEAD@{0} -- ${lockfile}`, {
     cwd: gitDir,
     encoding: "utf-8"
   });
 
-  if (YARNHOOK_DEBUG) {
-    console.log(output);
+  if (LOCKFILEHOOK_DEBUG) {
+    console.log(output, output.length > 0);
   }
 
-  // if diff exists, run yarn install
+  // if diff exists, update dependencies
   if (output.length > 0) {
-    const output = execSync("yarn install", { encoding: "utf-8" });
+    console.log(`Changes to lockfile found, running \`${CMD} install\``);
+    const output = execSync(`${CMD} install`, { encoding: "utf-8" });
     console.log(output);
   }
 }
