600221b213d96bd647b8987d83e814c45ac26306
Fatih Altinok <photonia@gmail.com>
2018-03-26 22:31:18 +0300

    Refactor a bit

--- index.js
+++ index.js
@@ -7,68 +7,65 @@ const execa = require("execa");
 const { join } = require("path");
 const fs = require("fs");
 
+// environment variables
 const { YARNHOOK_BYPASS = false, YARNHOOK_DEBUG = false, YARNHOOK_DRYRUN = false } = process.env;
 
+// supported package managers and lockfile names
+const lockfileSpecs = [
+  ["yarn", "yarn.lock"],
+  ["npm", "package-lock.json"],
+  ["pnpm", "shrinkwrap.yaml"]
+];
+
+function getLockfileSpec(currentDir) {
+  for (let [cmd, lockfile] of lockfileSpecs) {
+    const lockfilePath = join(currentDir, lockfile);
+    if (fs.existsSync(lockfilePath)) {
+      return { cmd, lockfilePath };
+    }
+  }
+
+  return null;
+}
+
 if (!YARNHOOK_BYPASS) {
-  // switch to gitdir
+  // find directories
   const currentDir = process.cwd();
   const gitDir = findParentDir.sync(currentDir, ".git");
-  const yarnLockPath = join(currentDir, "yarn.lock");
-  const npmLockPath = join(currentDir, "package-lock.json");
-  const pnpmLockPath = join(currentDir, "shrinkwrap.yaml");
 
-  // check for yarn's and npm's lockfiles
-  let CMD = "";
-  const getLockfile = function() {
-    // get yarn's lockfile
-    if (fs.existsSync(yarnLockPath)) {
-      CMD = "yarn";
-      return yarnLockPath;
-    }
-
-    // get npm's lockfile
-    if (fs.existsSync(npmLockPath)) {
-      CMD = "npm";
-      return npmLockPath;
-    }
-
-    // get pnpm's lockfile
-    if (fs.existsSync(pnpmLockPath)) {
-      CMD = "pnpm";
-      return pnpmLockPath;
-    }
-
-    return null;
-  };
-
-  const lockfile = getLockfile();
+  // check for lockfiles
+  const lockfileSpec = getLockfileSpec(currentDir);
 
   if (YARNHOOK_DEBUG) {
     console.log("currentDir:", currentDir);
     console.log("gitDir:", gitDir);
-    console.log("lockfile:", lockfile);
-    console.log("CMD:", CMD);
+    console.log("lockfile:", lockfileSpec);
   }
 
-  if (lockfile !== null) {
+  if (lockfileSpec !== null) {
+    // get the command and lockfile path
+    const { cmd, lockfilePath } = lockfileSpec;
+
     // run a git diff on the lockfile
-    const { stdout: output } = execa.sync("git", ["diff", "HEAD@{1}..HEAD@{0}", "--", lockfile], {
-      cwd: gitDir
-    });
+    const { stdout: output } = execa.sync(
+      "git",
+      ["diff", "HEAD@{1}..HEAD@{0}", "--", lockfilePath],
+      { cwd: gitDir }
+    );
 
     if (YARNHOOK_DEBUG) {
-      console.log(output, output.length > 0);
+      console.log(output);
     }
 
     // if diff exists, update dependencies
     if (output.length > 0) {
       if (YARNHOOK_DRYRUN) {
         console.log(
-          `Changes to lockfile found, you should run \`${CMD} install\` if you want to have dependencies of this branch.`
+          `Changes to lockfile found, you should run \`${cmd} install\` if you want to have dependencies of this branch.`
         );
       } else {
-        console.log(`Changes to lockfile found, running \`${CMD} install\``);
-        execa.sync(CMD, ["install"], { stdio: "inherit" });
+        console.log(`Changes to lockfile found, running \`${cmd} install\``);
+        execa.sync(cmd, ["install"], { stdio: "inherit" });
       }
     }
   } else {
