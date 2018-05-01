9ea6d51926f836eea3498df73d636661fbeec983
Fatih Altinok <photonia@gmail.com>
2018-05-01 17:10:54 +0300

    Initial lerna support

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ const { YARNHOOK_BYPASS = false, YARNHOOK_DEBUG = false, YARNHOOK_DRYRUN = false
 
 // supported package managers and lockfile names
 const lockfileSpecs = [
-  ["yarn", "yarn.lock"],
-  ["npm", "npm-shrinkwrap.json"],
-  ["npm", "package-lock.json"],
-  ["pnpm", "shrinkwrap.yaml"]
+  { command: "lerna", subcommand: "bootstrap", lockfileName: "lerna.json" },
+  { command: "yarn", subcommand: "install", lockfileName: "yarn.lock" },
+  { command: "npm", subcommand: "install", lockfileName: "npm-shrinkwrap.json" },
+  { command: "npm", subcommand: "install", lockfileName: "package-lock.json" },
+  { command: "pnpm", subcommand: "install", lockfileName: "shrinkwrap.yaml" }
 ];
 
 function getLockfileSpec(currentDir) {
-  for (let [cmd, lockfile] of lockfileSpecs) {
-    const lockfilePath = join(currentDir, lockfile);
+  for (let { command, subcommand, lockfileName } of lockfileSpecs) {
+    const lockfilePath = join(currentDir, lockfileName);
     if (fs.existsSync(lockfilePath)) {
-      return { cmd, lockfilePath };
+      return { command, subcommand, lockfilePath };
     }
   }
 
@@ -45,7 +46,7 @@ if (!YARNHOOK_BYPASS) {
 
   if (lockfileSpec !== null) {
     // get the command and lockfile path
-    const { cmd, lockfilePath } = lockfileSpec;
+    const { command, subcommand, lockfilePath } = lockfileSpec;
 
     // run a git diff on the lockfile
     const { stdout: output } = execa.sync(
@@ -62,11 +63,11 @@ if (!YARNHOOK_BYPASS) {
     if (output.length > 0) {
       if (YARNHOOK_DRYRUN) {
         console.log(
-          `Changes to lockfile found, you should run \`${cmd} install\` if you want to have dependencies of this branch.`
+          `Changes to lockfile found, you should run \`${command} ${subcommand}\` if you want to have dependencies of this branch.`
         );
       } else {
-        console.log(`Changes to lockfile found, running \`${cmd} install\``);
-        execa.sync(cmd, ["install"], { stdio: "inherit" });
+        console.log(`Changes to lockfile found, running \`${command} ${subcommand}\``);
+        execa.sync(command, [subcommand], { stdio: "inherit" });
       }
     }
   } else {
