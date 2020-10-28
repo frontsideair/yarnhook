cf4ff936357c71fe946b908989d2f1bbc0e915ba
Fatih Altinok <photonia@gmail.com>
2020-10-29 00:09:12 +0300

    Speed up and clean up tests

--- test.js
+++ test.js
@@ -3,10 +3,11 @@ const path = require("path");
 
 const NEW_BRANCH = "new-branch";
 const MAIN_BRANCH = "main";
-const TIMEOUT = 60 * 1000;
 const TEST_DIRECTORY = "test";
 const REMOTE_REPO_PATH = path.join(TEST_DIRECTORY, "remote");
 const LOCAL_REPO_PATH = path.join(TEST_DIRECTORY, "local");
+
+jest.setTimeout(60 * 1000);
 
 async function cmd(command, cwd) {
   const { stdout } = await execa.command(command, { cwd, shell: true });
@@ -22,7 +23,7 @@ async function remoteCmd(command) {
 }
 
 async function installYarnhook() {
-  await remoteCmd("npm install --save-dev yarnhook husky");
+  await remoteCmd("npm install --save-dev --package-lock-only ../../packages/yarnhook husky");
   await remoteCmd(
     `npx json -I -f package.json -e 'this.husky={"hooks":{"post-checkout":"yarnhook $HUSKY_GIT_PARAMS","post-merge":"yarnhook $HUSKY_GIT_PARAMS","post-rewrite":"yarnhook $HUSKY_GIT_PARAMS"}}'`
   );
@@ -38,7 +39,7 @@ async function initialize() {
   await remoteCmd("git add package.json package-lock.json index.js");
   await remoteCmd(`git commit -m "Initial commit"`);
   await remoteCmd(`git checkout -b ${NEW_BRANCH}`);
-  await remoteCmd("npm install --save number-zero");
+  await remoteCmd("npm install --save --package-lock-only number-zero");
   await remoteCmd(`echo "console.log(require('number-zero'))" > index.js`);
   await remoteCmd("git add package.json package-lock.json index.js");
   await remoteCmd(`git commit -m "Add number-zero"`);
@@ -48,14 +49,14 @@ async function cleanup() {
   await cmd(`rm -rf ${TEST_DIRECTORY}`);
 }
 
-beforeAll(initialize, TIMEOUT);
+beforeAll(initialize);
 
-afterAll(cleanup, TIMEOUT);
+afterAll(cleanup);
 
 beforeEach(async () => {
   await cmd(`git clone ${REMOTE_REPO_PATH} ${LOCAL_REPO_PATH} --branch=${MAIN_BRANCH}`);
-  await localCmd("npm install");
-}, TIMEOUT);
+  await localCmd("npm ci");
+});
 
 afterEach(async () => {
   await cmd(`rm -rf ${LOCAL_REPO_PATH}`);
@@ -63,12 +64,10 @@ afterEach(async () => {
 
 describe("smoke test", () => {
   it("should ensure dependencies are up-to-date on branch change", async () => {
-    let output = await localCmd("node index.js");
-    expect(output).toBe("1");
+    await expect(localCmd("node index.js")).resolves.toBe("1");
 
     await localCmd(`git checkout ${NEW_BRANCH}`);
-    output = await localCmd("node index.js");
-    expect(output).toBe("0");
+    await expect(localCmd("node index.js")).resolves.toBe("0");
   });
 
   it("should not fail when the first non-branch-changing checkout is done", async () => {
